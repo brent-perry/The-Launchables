e78d47f6667be4ef20aa9625809f4f2bef6a49ea
brent-perry <53350346+brent-perry@users.noreply.github.com>
2021-12-06 08:43:47 -0800

    made amou counter count on player respawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,17 @@ public class GameManager : MonoBehaviour
     bool lossLevel = false;
     bool wonLevel = false;
     GameOver _gameOver;
+    PlayerController _playerController;
 
     void Start()
     {
+        _playerController = FindObjectOfType<PlayerController>();
         _gameOver = FindObjectOfType<GameOver>();
     }
 
     void Update()
     {
-        if (AmmoController.Ammo <= 0 && !wonLevel)
+        if (AmmoController.Ammo <= 0 && !wonLevel && _playerController.playerRigidbody2D.position == _playerController.playerStartPosition)
         {
             _gameOver.EndGame();
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,27 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     AmmoController _ammoController;
-    Rigidbody2D _rigidbody2D;
+    public Rigidbody2D playerRigidbody2D;
     SpriteRenderer _spriteRenderer;
     LineRenderer _lineRenderer;
     Vector2 _currentPosition;
-    Vector2 _startPosition;
+    public Vector2 playerStartPosition;
     [SerializeField] float _launchPower = 1000;
     [SerializeField] float _maxDragDistance = 4f;
+    bool _hasFired;
 
     void Awake()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _lineRenderer = GetComponent<LineRenderer>();
     }
     void Start()
     {
+        _hasFired = false;
         _ammoController = FindObjectOfType<AmmoController>();
-        _startPosition = _rigidbody2D.position;
-        _rigidbody2D.isKinematic = true; //prevents player object from moving
+        playerStartPosition = playerRigidbody2D.position;
+        playerRigidbody2D.isKinematic = true; //prevents player object from moving
     }
 
     void Update()
@@ -44,11 +46,11 @@ public class PlayerController : MonoBehaviour
     void OnMouseUp()
     {
         _spriteRenderer.color = Color.white; //changes player back to original color upon release
-        _currentPosition = _rigidbody2D.position; //gets the current position of the player object
-        Vector2 direction = _startPosition - _currentPosition; //determines which directions to send the object
+        _currentPosition = playerRigidbody2D.position; //gets the current position of the player object
+        Vector2 direction = playerStartPosition - _currentPosition; //determines which directions to send the object
         direction.Normalize(); //keeps the same direction but its length is 1.0.
-        _rigidbody2D.isKinematic = false; //allows object to move again
-        _rigidbody2D.AddForce(direction * _launchPower); //pushes object in opposite direction of the mouse multiplied by the _launchPower
+        playerRigidbody2D.isKinematic = false; //allows object to move again
+        playerRigidbody2D.AddForce(direction * _launchPower); //pushes object in opposite direction of the mouse multiplied by the _launchPower
         _lineRenderer.enabled = false; //removes the line renderer(arrows) upon release of mouse
         _ammoController.AmmoCounter(); //removes 1 ammo
     }
@@ -58,20 +60,20 @@ public class PlayerController : MonoBehaviour
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 desiredPosition = mousePosition;
 
-        float distance = Vector2.Distance(desiredPosition, _startPosition);
+        float distance = Vector2.Distance(desiredPosition, playerStartPosition);
         if (distance > _maxDragDistance)
         {
-            Vector2 direction = desiredPosition - _startPosition;
+            Vector2 direction = desiredPosition - playerStartPosition;
             direction.Normalize();
-            desiredPosition = _startPosition + (direction * _maxDragDistance);//makes the clamping more of a circular distance instead of a wall
+            desiredPosition = playerStartPosition + (direction * _maxDragDistance);//makes the clamping more of a circular distance instead of a wall
         }
 
-        if (desiredPosition.x > _startPosition.x)
+        if (desiredPosition.x > playerStartPosition.x)
         {
-            desiredPosition.x = _startPosition.x; //clamps the object so player can not drag to the right
+            desiredPosition.x = playerStartPosition.x; //clamps the object so player can not drag to the right
         }
 
-        _rigidbody2D.position = desiredPosition;
+        playerRigidbody2D.position = desiredPosition;
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -82,16 +84,16 @@ public class PlayerController : MonoBehaviour
     void LineRendererAimer()
     {
         _lineRenderer.SetPosition(0, transform.position);
-        _lineRenderer.SetPosition(1, _startPosition);
+        _lineRenderer.SetPosition(1, playerStartPosition);
     }
 
     IEnumerator ResetAfterDelay()
     {
         yield return new WaitForSeconds(2);
-        _rigidbody2D.position = _startPosition;
-        _rigidbody2D.isKinematic = true;
-        _rigidbody2D.velocity = Vector2.zero;
+        playerRigidbody2D.position = playerStartPosition;
+        playerRigidbody2D.isKinematic = true;
+        playerRigidbody2D.velocity = Vector2.zero;
     }
 
-    
+
 }
